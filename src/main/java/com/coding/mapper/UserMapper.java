f0853c5bbf74d455991702234e24e3e5f8378310
xhu <xhusoul@sina.com>
2017-10-30 15:39:16 +0800

    用户管理模块 pojo 、mapper 逆向工程生成

--- src/main/java/com/coding/mapper/UserMapper.java
+++ src/main/java/com/coding/mapper/UserMapper.java
@@ -9,30 +9,30 @@ public interface UserMapper {
      * 根据用户 ID 删除用户信息
      * @param userUuid
      */
-    public void deleteByPrimaryKey(String userUuid);
+    public void deleteUserByPrimaryKey(String userUuid);
 
     /**
      * 插入一条用户信息
      * @param user
      */
-    public void insert(User user);
+    public void insertUser(User user);
 
     /**
      * 通过用户唯一 ID 查询用户信息
      * @param userUuid
      * @return
      */
-    public User selectByPrimaryKey(String userUuid);
+    public User selectUserByPrimaryKey(String userUuid);
 
     /**
      * 根据用户唯一 ID 更新用户信息
      * @param user
      */
-    public void  updateByPrimaryKey(User user);
+    public void  updateUserByPrimaryKey(User user);
 
     /**
      * 查询所有用户信息
      * @return
      */
-    public List<User> selectAll();
+    public List<User> selectUserAll();
 }
