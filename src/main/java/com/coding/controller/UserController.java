7e5c203be448dfc78dc527e9e482c716347b320a
zerow9 <825323237@qq.com>
2017-10-30 21:52:25 +0800

    Controller Commit

--- src/main/java/com/coding/controller/UserController.java
+++ src/main/java/com/coding/controller/UserController.java
@@ -4,9 +4,12 @@ import com.coding.Iservice.UserService;
 import com.coding.pojo.Address;
 import com.coding.pojo.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+@Controller
+@RequestMapping("admin")
 public class UserController {
     @Autowired
     private UserService userService;
