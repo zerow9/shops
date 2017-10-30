7868da19757e5724d8f5c4c07455ba8233e9c74c
xhu <xhusoul@sina.com>
2017-10-30 18:53:38 +0800

    地址表的增删

--- src/main/java/com/coding/controller/Test.java
+++ src/main/java/com/coding/controller/Test.java
@@ -1,4 +1,21 @@
 package com.coding.controller;
 
+import com.coding.mapper.AddressMapper;
+import com.coding.pojo.Address;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
 public class Test {
+    @Autowired
+    private AddressMapper addressMapper;
+
+    @RequestMapping("test")
+    public void selectAddressAll(){
+        System.out.println("Controller~~~");
+        List<Address> addressList = addressMapper.selectAddressAll();
+        System.out.println(addressList);
+    }
+
 }
