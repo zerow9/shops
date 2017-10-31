1ae60f871f4d06229ff07cad9f5f108bf7139cf7
彭强 <32957718+1124891553@users.noreply.github.com>
2017-11-01 00:57:39 +0800

    异常抛出优化.

--- src/main/test/com/coding/mapper/UserMapperTest.java
+++ src/main/test/com/coding/mapper/UserMapperTest.java
@@ -1,5 +1,6 @@
 package com.coding.mapper;
 import com.coding.Iservice.IAdminService;
+import com.coding.pojo.User;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,5 +15,42 @@ public class UserMapperTest {
     @Test
     public void selectUserAll() throws Exception {
         System.out.println(adminService.selectUserAll());
+    }
+    @Test
+    public void selectUserByPrimaryKey() throws Exception {
+        System.out.println(adminService.selectUserByPrimaryKey("1"));
+    }
+    @Test
+    public void insertUser() throws Exception {
+        User user = new User();
+        user.setUserUuid("3");
+        user.setUserPhone("123");
+        user.setUserPassword("123");
+        adminService.insertUser(user);
+    }
+    @Test
+    public void updateUserByPrimaryKey() throws Exception {
+        User user = new User();
+        user.setUserUuid("3");
+        user.setUserPhone("123456");
+        user.setUserPassword("123");
+        adminService.updateUserByPrimaryKey(user);
+    }
+    @Test
+    public void deleteUserByPrimaryKey() throws Exception {
+        adminService.deleteUserByPrimaryKey("3");
+    }
+
+    @Test
+    public void selectAddressAll() throws Exception {
+        System.out.println(adminService.selectAddressAll());
+    }
+    @Test
+    public void selectAddressByPrimaryKey() throws Exception {
+        System.out.println(adminService.selectAddressByPrimaryKey(1));
+    }
+    @Test
+    public void selectAddressByUserID() throws Exception {
+        System.out.println(adminService.selectAddressByUserID("4"));
     }
 }
