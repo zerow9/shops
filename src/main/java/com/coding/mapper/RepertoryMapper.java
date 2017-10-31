6d32e8f2e15338d98c642e4e314c63b588e27724
xhu <xhusoul@sina.com>
2017-10-31 16:52:51 +0800

    item repertory 表的 增删改查  测试成功

--- src/main/java/com/coding/mapper/RepertoryMapper.java
+++ src/main/java/com/coding/mapper/RepertoryMapper.java
@@ -1,17 +1,41 @@
 package com.coding.mapper;
 
 import com.coding.pojo.Repertory;
+import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 
 public interface RepertoryMapper {
-    public void deleteRepertoryByPrimaryKey(Integer repertoryId);
+    /**
+     * 根据库存ID删除库存信息
+     *
+     * @param repertoryId 库存ID
+     * @throws Exception
+     */
+    public void deleteRepertoryByPrimaryKey(Integer repertoryId) throws Exception;
 
-    public void insertRepertory(Repertory repertory);
+    /**
+     * @param repertory 封装了库存信息的 Repertory 类对象
+     * @throws Exception
+     */
+    public void insertRepertory(Repertory repertory) throws Exception;
 
-    public Repertory selectRepertoryByPrimaryKey(Integer repertoryId);
+    /**
+     * @param repertoryId 库存ID
+     * @return 封装了库存信息的 Repertory 类对象
+     * @throws Exception
+     */
+    public Repertory selectRepertoryByPrimaryKey(@Param("repertoryId") Integer repertoryId) throws Exception;
 
-    public void updateRepertoryByPrimaryKey(Repertory repertory);
+    /**
+     * @param repertory 封装了库存信息的 Repertory 类对象
+     * @throws Exception
+     */
+    public void updateRepertoryByPrimaryKey(Repertory repertory) throws Exception;
 
-    public List<Repertory> sellectRepertoryAll();
+    /**
+     * @return 装了库存信息的 Repertory 类对象集合
+     * @throws Exception
+     */
+    public List<Repertory> selectRepertoryAll() throws Exception;
 }
