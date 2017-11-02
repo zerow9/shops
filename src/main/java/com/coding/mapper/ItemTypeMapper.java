5859c20365b6eb8a8e3236c5d2fa73d02e1ea55f
xhu <xhusoul@sina.com>
2017-11-02 14:55:19 +0800

    对模糊查询的参数进行限制

--- src/main/java/com/coding/mapper/ItemTypeMapper.java
+++ src/main/java/com/coding/mapper/ItemTypeMapper.java
@@ -23,6 +23,7 @@ public interface ItemTypeMapper {
     public int insertItemType(ItemType itemType) throws Exception;
 
     /**
+     * 通过上类品类别ID查询商品类别信息
      * @param itemTypeId 商品类别ID
      * @return 封装了商品类别信息的 ItemType 类对象
      * @throws Exception
@@ -31,12 +32,14 @@ public interface ItemTypeMapper {
     public ItemType selectItemTypeByPrimaryKey(@Param("itemTypeId") Integer itemTypeId) throws Exception;
 
     /**
+     * 通过商品类别ID更新商品类别信息
      * @param itemType 封装了商品类别信息的 ItemType 类对象
      * @throws Exception
      */
     public int updateItemTypeByPrimaryKey(ItemType itemType) throws Exception;
 
     /**
+     * 查询所有商品类别信息
      * @return 封装了商品类别信息的 ItemType 类对象集合
      * @throws Exception
      */
