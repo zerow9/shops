1bdc98f229fd19f9dcba9e6350ca427962c4d971
文小成 <634650981@qq.com>
2017-11-09 11:09:35 +0800

    update

--- src/main/java/com/coding/controller/OrderController.java
+++ src/main/java/com/coding/controller/OrderController.java
@@ -49,103 +49,16 @@ public class OrderController {
         return jsonObject.toString();
     }
 
-    /**
-     * 插入一条用户信息(注册)
-     *
-     * @param user 新用户
-     */
-//    @RequestMapping("insertUser")
-//    public boolean insertUser(User user) throws Exception {
-//        System.out.println(user);
-//        user.setUserRegisterDateTime(new Date());
-//        user.setUserAge(11);
-//        user.setUserLandNumber(11);
-//        user.setUserCurrentTime(new Date());
-//        user.setUserLandIp(InetAddress.getLocalHost().getHostAddress());
-//        user.setUserUuid(MyUUID.randomUUID());
-//        user.setUserAddress(1);
-//        userService.insertUser(user);
-//        return true;
-//    }
-
-    /**
-     * 根据用户ID查询用户信息(登录)
-     *
-     * @param userUuid 用户的uuid
-     */
-    @RequestMapping("selectUserByPrimaryKey")
-    public String selectUserByPrimaryKey(String userUuid) throws Exception {
-        User user = userService.selectUserByPrimaryKey(userUuid);
-        return "";
+    @ResponseBody
+    @RequestMapping("selectOrderByPrimaryKey")
+    public String selectOrderByPrimaryKey(String orderId) {
+        try {
+            Orders orders = userService.selectOrderByPrimaryKey(Integer.parseInt(orderId));
+            JSONObject jsonObject = JSONObject.fromObject(orders);
+            return jsonObject.toString();
+        } catch (Exception e) {
+            return e.getMessage();
+        }
     }
 
-    /**
-     * 根据用户ID修改用户信息
-     *
-     * @param userUuid 更新用户的信息
-     */
-
-
-    /**
-     * 根据orderId删除订单信息
-     *
-     * @param orderId 订单编号
-     */
-    @RequestMapping("deleteOrderByPrimaryKey")
-    public String deleteOrderByPrimaryKey(Integer orderId) throws Exception {
-        userService.deleteAddressByPrimaryKey(orderId);
-        return "";
-    }
-
-    /**
-     * 插入一条地址信息
-     *
-     * @param address 用户的收货地址
-     * @param uuid    用户的uuid
-     */
-    @RequestMapping("insertAddress")
-    public String insertAddress(Address address, String uuid) throws Exception {
-        address.setUserUuid(uuid);
-        userService.insertAddress(address);
-        return "";
-    }
-
-    /**
-     * 查询用户地址
-     *
-     * @param userUuid 根据用户uuid查询用户地址
-     */
-    @RequestMapping("selectAddressByUserID")
-    public String selectAddressByUserID(String userUuid) throws Exception {
-        List<Address> address = userService.selectAddressByUserID(userUuid);
-        return "";
-    }
-
-    /**
-     * 根据用户ID更新地址信息
-     *
-     * @param address  更新地址信息
-     * @param userUuid 根据用户的uuid来更新地址
-     */
-    @RequestMapping("updateAddressByPrimaryKey")
-    public String updateAddressByPrimaryKey(Address address, String userUuid) throws Exception {
-        address.setUserUuid(userUuid);
-        userService.updateAddressByPrimaryKey(address);
-        return "";
-    }
-
-
-    @RequestMapping("addUser")
-    public String addUser() {
-        return "users/adduser";
-    }
-
-
-    @RequestMapping("seeUserIdByKey")
-    public String seeUserIdByKey(String userUuid, Model model) throws Exception {
-        User user = userService.selectUserByPrimaryKey(userUuid);
-        user.setDateToString(DateToString.date(user.getUserRegisterDateTime()));
-        model.addAttribute("user", user);
-        return "users/seeUser";
-    }
 }
--- src/main/webapp/js/order/order_list.js
+++ src/main/webapp/js/order/order_list.js
@@ -4,7 +4,7 @@ layui.use('table', function () {
     // 方法级渲染表格
     var tableObj = table.render({
         elem: '#layui_table'    //绑定元素
-        , url: 'http://localhost:8080/order/selectOrder.action'   //资源地址
+        , url: '/order/selectOrder.action'   //资源地址
         , id: 'idTest'   //设定容器唯一ID
         , height: '500'
         , page: true    //开启分页
@@ -26,12 +26,12 @@ layui.use('table', function () {
             console.log('数据总量：' + count);     //数据总量
         }
         , initSort: {   //初始排序
-            field: 'order_id' //排序字段，对应 cols 设定的各字段名
+            field: 'orderId' //排序字段，对应 cols 设定的各字段名
             , type: 'asc' //排序方式  asc: 升序、desc: 降序、null: 默认排序
         }
         // 每页数据量可选项
         , limits: [10, 20, 30, 50, 100, 200, 500]
-        , limit: 10 //每页默认显示的数量
+        , limit: 7 //每页默认显示的数量
         , skin: 'line' //行边框风格
         , even: true //开启隔行背景
         , size: 'lg'  //设定表格尺寸
@@ -41,39 +41,55 @@ layui.use('table', function () {
     table.on('tool(demo)', function (obj) {
         var data = obj.data;
         if (obj.event === 'detail') {        // 查看详情
-            var order_items = '' +
-                '<div class="detail_info">' +
-                '<table class="layui-table" lay-skin="nob" style="color: black">' +
-                '<colgroup>' +
-                '<col width="150">' +
-                '<col width="200">' +
-                '</colgroup>' +
-                '<tbody>' +
-                '<tr><td style="text-align: right;font-weight: bold">订单编号：</td>' + '<td>' + data.order_id + '</td></tr>' +
-                '<tr><td style="text-align: right;font-weight: bold">收件人：</td>' + '<td>' + data.take_goods_name + '</td></tr>' +
-                '<tr><td style="text-align: right;font-weight: bold">分店：</td>' + '<td>' + data.shop_name + '</td></tr>' +
-                '<tr><td style="text-align: right;font-weight: bold">订单总额：</td>' + '<td>' + data.order_sum_price + '</td></tr>' +
-                '<tr><td style="text-align: right;font-weight: bold">支付状态：</td>' + '<td>' + data.pay_status + '</td></tr>' +
-                '<tr><td style="text-align: right;font-weight: bold">发货状态：</td>' + '<td>' + data.send_status + '</td></tr>' +
-                '<tr><td style="text-align: right;font-weight: bold">下单时间：</td>' + '<td>' + data.order_create_time + '</td></tr>' +
-                '</tbody>' +
-                '</table>' +
-                '</div>';
-            parent.layer.open({
-                type: 1
-                , title: ['订单详情', 'font-size:18px;']
-                , closeBtn: 1
-                , area: ['auto', 'auto']
-                , shade: 0.3  //遮罩
-                , id: 'del_order_id' //设定一个id，防止重复弹出
-                , btn: ['关闭']
-                , btn1: function (index, layero) {  //取消按钮的操作
-                    parent.layer.close(index);
+            $.ajax({
+                type: "POST",
+                url: "/order/selectOrderByPrimaryKey.action",
+                data: {'orderId': data.orderId},
+                timeout: 10000, //超时时间：10秒
+                success: function (result) {
+                    console.log(result);
+                    if (result === 'true') {
+                        var order_items = '' +
+                            '<div class="detail_info">' +
+                            '<table class="layui-table" lay-skin="nob" style="color: black">' +
+                            '<colgroup>' +
+                            '<col width="150">' +
+                            '<col width="200">' +
+                            '</colgroup>' +
+                            '<tbody>' +
+                            '<tr><td style="text-align: right;font-weight: bold">订单编号：</td>' + '<td>' + data.order_id + '</td></tr>' +
+                            '<tr><td style="text-align: right;font-weight: bold">收件人：</td>' + '<td>' + data.take_goods_name + '</td></tr>' +
+                            '<tr><td style="text-align: right;font-weight: bold">分店：</td>' + '<td>' + data.shop_name + '</td></tr>' +
+                            '<tr><td style="text-align: right;font-weight: bold">订单总额：</td>' + '<td>' + data.order_sum_price + '</td></tr>' +
+                            '<tr><td style="text-align: right;font-weight: bold">支付状态：</td>' + '<td>' + data.pay_status + '</td></tr>' +
+                            '<tr><td style="text-align: right;font-weight: bold">发货状态：</td>' + '<td>' + data.send_status + '</td></tr>' +
+                            '<tr><td style="text-align: right;font-weight: bold">下单时间：</td>' + '<td>' + data.order_create_time + '</td></tr>' +
+                            '</tbody>' +
+                            '</table>' +
+                            '</div>';
+                        parent.layer.open({
+                            type: 1
+                            , title: ['订单详情', 'font-size:18px;']
+                            , closeBtn: 1
+                            , area: ['auto', 'auto']
+                            , shade: 0.3  //遮罩
+                            , id: 'del_order_id' //设定一个id，防止重复弹出
+                            , btn: ['关闭']
+                            , btn1: function (index, layero) {  //取消按钮的操作
+                                parent.layer.close(index);
+                            }
+                            , move: false
+                            , btnAlign: 'c' //按钮居中对齐
+                            , content: order_items
+                            , shadeClose: true
+                        });
+                    } else {
+                        parent.layer.alert(result.toString());
+                    }
+                },
+                error: function (e) {
+                    parent.layer.alert("请求数据时出错：" + e.statusText)
                 }
-                , move: false
-                , btnAlign: 'c' //按钮居中对齐
-                , content: order_items
-                , shadeClose: true
             });
         } else if (obj.event === 'del') {   //删除单行操作
             parent.layer.open({     // 弹出确认删除对话框
@@ -92,7 +108,7 @@ layui.use('table', function () {
                     $.ajax({
                         type: "POST",
                         url: "deleteOrderByPrimaryKey.action",
-                        data: {'orderId': data.order_id},
+                        data: {'orderId': data.orderId},
                         timeout: 10000, //超时时间：10秒
                         success: function (result) {
                             if (result.match('Unknown')) {
