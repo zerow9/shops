4f3c286b4212b558c5e1b3d98c0ef081490a7396
xhu <xhusoul@sina.com>
2017-11-14 15:11:02 +0800

    PayType 表的 增删改查定义

---
+++ src/main/java/com/coding/paging/PagingCustomPayType.java
@@ -0,0 +1,15 @@
+package com.coding.paging;
+
+import com.coding.pojo.PayType;
+
+public class PagingCustomPayType extends Paging{
+    private PayType payType;
+
+    public PayType getPayType() {
+        return payType;
+    }
+
+    public void setPayType(PayType payType) {
+        this.payType = payType;
+    }
+}
---
+++ src/main/java/com/coding/pojo/PayType.java
@@ -0,0 +1,32 @@
+package com.coding.pojo;
+
+public class PayType {
+    private Integer payTypeId;
+
+    private String payTypeName;
+
+
+    public Integer getPayTypeId() {
+        return payTypeId;
+    }
+
+    public void setPayTypeId(Integer payTypeId) {
+        this.payTypeId = payTypeId;
+    }
+
+    public String getPayTypeName() {
+        return payTypeName;
+    }
+
+    public void setPayTypeName(String payTypeName) {
+        this.payTypeName = payTypeName == null ? null : payTypeName.trim();
+    }
+
+    @Override
+    public String toString() {
+        return "PayType{" +
+                "payTypeId=" + payTypeId +
+                ", payTypeName='" + payTypeName + '\'' +
+                '}';
+    }
+}
