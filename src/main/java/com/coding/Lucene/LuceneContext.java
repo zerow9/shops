9cd3645060c693a5772ea3fd17318a30e73dcb84
彭强 <32957718+1124891553@users.noreply.github.com>
2017-11-23 11:23:20 +0800

    Lucene 索引增删改查实现.

--- src/main/java/com/coding/Lucene/LuceneContext.java
+++ src/main/java/com/coding/Lucene/LuceneContext.java
@@ -34,9 +34,7 @@ public class LuceneContext {
     //单列初始化操作
     private static void init() throws IOException {
         String dicUrl = new File(".").getCanonicalPath();
-        String pathString  = dicUrl.substring(0,dicUrl.lastIndexOf(File.separator));
-        System.out.println(pathString+File.separator+"index");
-        directory = FSDirectory.open(new File(pathString+File.separator+"index"));
+        directory = FSDirectory.open(new File(dicUrl+File.separator+"index"));
         version = Version.LUCENE_35;
         analyzer = new StandardAnalyzer(version);
         if(writer == null)
